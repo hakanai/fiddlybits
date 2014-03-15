a12fcf363dd15c6b70ffdbfbb61d4ee20cac6f78
Trejkaz <trejkaz@trypticon.org>
2014-03-15 15:24:19 +1100

    Initial Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fiddlybits::Application.config.secret_key_base = '8b7522611f82737292672067ea1b7e8086011494fa3ed7526d3ec043544bb6fe6e356d671a2fe944a6983fc2b4e3c47cf041ec10d0e723e28e498e35e7038b6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fiddlybits::Application.config.session_store :cookie_store, key: '_fiddlybits_session'
