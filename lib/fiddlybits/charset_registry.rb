6fa4c8ea80d00cbe377d6a25e8f8f624d3dc0681
Trejkaz <trejkaz@trypticon.org>
2014-04-24 23:00:52 +1000

    Sort charset names better

--- lib/fiddlybits/charset_registry.rb
+++ lib/fiddlybits/charset_registry.rb
@@ -8,9 +8,10 @@ module Fiddlybits
       if !@all
         all = []
         [ Iso2022Charset, TableCharset, AsciiCharset ].each do |cls|
-          all += cls.constants.sort.map { |c| cls.const_get(c) }.select { |v| v.is_a?(Charset) }
+          all += cls.constants.map { |c| cls.const_get(c) }.select { |v| v.is_a?(Charset) }
         end
-        all.sort_by! { |cs| cs.name }
+        # Sorting alphanumerics sensibly.
+        all.sort_by! { |cs| cs.name.gsub(/\d+/) { |n| "%05d" % n.to_i } }
         @all = all
       end
 
