fbef6f74c623590bcf55c99cf186a3763e697bf3
Trejkaz <trejkaz@trypticon.org>
2014-05-12 00:20:28 +1000

    Load the share button sooner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def share_button
+    str = <<END
+      <div id="share-button-top" class="share-button share-button-top"></div>
+      <script type="text/javascript">new Share(".share-button", { ui: { flyout: "bottom left", button_background: "rgb(128,160,160)", button_color: "rgb(0,39,39)" } });</script>
+END
+    str.html_safe
+  end
 end
