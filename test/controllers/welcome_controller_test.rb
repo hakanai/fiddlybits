b76f16766d573874aecc5e1dd4bbf67b6281d604
Trejkaz (pen name) <trejkaz@trypticon.org>
2017-05-30 22:57:44 +1000

    Test for welcome controller - page seems to load file. :/

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "loading the page" do
+    get 'index'
+    assert_response 200
+  end
 end
